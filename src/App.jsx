4db90402676aa484cc7b7350a28e69eb0db5167c
zerotwosss188-ship-it <zerotwosss188@gmail.com>
2026-09-27 01:37:00 +0500

    Scroll to top on route change

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import NewsTicker from './components/NewsTicker';
 import Chatbot from './components/Chatbot';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
 import CategoryHub from './pages/CategoryHub';
 import ContentDetail from './pages/ContentDetail';
@@ -12,11 +14,11 @@ import Contact from './pages/Contact';
 import Bookmarks from './pages/Bookmarks';
 import Releases from './pages/Releases';
 import Trailers from './pages/Trailers';
-import BookmarkToast from './components/BookmarkToast';
 
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <div style={{ position: 'relative', zIndex: 1 }}>
         <a href="#main-content" className="fv-skip-link">
           Skip to main content
@@ -37,10 +39,10 @@ export default function App() {
           </Routes>
         </main>
         <div style={{ marginTop: 60 }}>
+          <NewsTicker />
         </div>
         <Footer />
         <Chatbot />
-        <BookmarkToast /> 
       </div>
     </BrowserRouter>
   );
